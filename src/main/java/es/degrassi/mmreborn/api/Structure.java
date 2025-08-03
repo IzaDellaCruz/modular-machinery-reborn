7106edcdf4247574ae58ef76b3b392f1de9cb4cd
alec016 <aaronquemadadiaz@gmail.com>
2025-08-03 18:18:25 +0200

    fix not placing structure

--- src/main/java/es/degrassi/mmreborn/api/Structure.java
+++ src/main/java/es/degrassi/mmreborn/api/Structure.java
@@ -42,7 +42,7 @@ public class Structure {
   ).apply(structure, Structure::makeStructure), "Structure with modifiers");
 
   public static final Structure EMPTY = new Structure(Map.of(), List.of(List.of("m")), Map.of(), List.of());
-  private static final RandomSource random = RandomSource.create();
+  private static final RandomSource random = RandomSource.create(42L);
 
   private static Structure makeStructure(List<List<String>> pattern, Map<Character, BlockIngredient> keys, List<ModifierReplacement> modifiers) {
     Structure.Builder builder = Structure.Builder.start();
@@ -141,7 +141,7 @@ public class Structure {
         continue;
       }
       if (worldPos.equals(controllerPos)) continue;
-      setBlock(level, worldPos, ingredient.getAll().get(random.nextInt() * ingredient.getAll().size()));
+      setBlock(level, worldPos, ingredient.getAll().get(random.nextInt(0, ingredient.getAll().size())));
     }
   }
 
