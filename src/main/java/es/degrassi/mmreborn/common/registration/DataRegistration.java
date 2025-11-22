da9ccae87e0f117d8ddd50cb04e7c5f53d867580
alec016 <aaronquemadadiaz@gmail.com>
2025-11-22 19:57:41 +0100

    add: resource location direct sync with GUI

--- src/main/java/es/degrassi/mmreborn/common/registration/DataRegistration.java
+++ src/main/java/es/degrassi/mmreborn/common/registration/DataRegistration.java
@@ -11,6 +11,7 @@ import es.degrassi.mmreborn.api.network.data.IntegerData;
 import es.degrassi.mmreborn.api.network.data.ItemStackData;
 import es.degrassi.mmreborn.api.network.data.LongData;
 import es.degrassi.mmreborn.api.network.data.NbtData;
+import es.degrassi.mmreborn.api.network.data.ResourceLocationData;
 import es.degrassi.mmreborn.api.network.data.StringData;
 import es.degrassi.mmreborn.api.network.syncable.BooleanSyncable;
 import es.degrassi.mmreborn.api.network.syncable.DoubleSyncable;
@@ -20,9 +21,11 @@ import es.degrassi.mmreborn.api.network.syncable.IntegerSyncable;
 import es.degrassi.mmreborn.api.network.syncable.ItemStackSyncable;
 import es.degrassi.mmreborn.api.network.syncable.LongSyncable;
 import es.degrassi.mmreborn.api.network.syncable.NbtSyncable;
+import es.degrassi.mmreborn.api.network.syncable.ResourceLocationSyncable;
 import es.degrassi.mmreborn.api.network.syncable.StringSyncable;
 import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.fluids.FluidStack;
@@ -56,6 +59,8 @@ public class DataRegistration {
       () -> DataType.create(Long.class, LongSyncable::create, LongData::new));
   public static final Supplier<DataType<NbtData, CompoundTag>> NBT_DATA = DATAS.register(rootLC("nbt"),
       () -> DataType.create(CompoundTag.class, NbtSyncable::create, NbtData::new));
+  public static final Supplier<DataType<ResourceLocationData, ResourceLocation>> RESOURCE_LOCATION_DATA= DATAS.register(rootLC("rl"),
+      () -> DataType.create(ResourceLocation.class, ResourceLocationSyncable::create, ResourceLocationData::new));
 
 
   public static void register(final IEventBus bus) {
